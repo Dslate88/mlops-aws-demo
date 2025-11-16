c52d3bc9cdaa61e75144e4f5e52ee46e3d34e32a
Devin Slate <sl8t3@hotmail.com>
2025-11-16 12:30:33 -0700

    initial baml setup for mlflow

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,14 +1,21 @@
+import json
 from fastapi import FastAPI
 import mlflow
 from mlflow.tracking import MlflowClient
+from dotenv import load_dotenv
+from .baml_client import b
+from mlflow.entities.model_registry import RegisteredModel
+from google.protobuf.json_format import MessageToDict
+
+
+load_dotenv()
 
 MLFLOW_TRACKING_URI="http://127.0.0.1:5000/"
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment("Default")
 
 client = MlflowClient()
-
-models = client.search_registered_models()
+# models = client.search_registered_models()
 
 app = FastAPI(title="ML_LLM Ops Demo - Backend APIs", version="0.0.1")
 
@@ -19,5 +26,11 @@ def home():
 @app.get("/list_models")
 def list_models():
     models = client.search_registered_models()
-    names = [model.name for model in models]
-    return {"names": names}
+
+    s = [
+        MessageToDict(m.to_proto(), preserving_proto_field_name=True)
+        for m in models
+    ]
+
+    response = b.ExtractModelInfo(json.dumps(s))
+    return {"models": response} 
